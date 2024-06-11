626c36d32a0fc418dfda700b766b0dfef5953dbb
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-11 14:22:20 +0200

    feat: add array, take di element in the page, and set the "while"

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 // Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
 
+// creo una lista della spesa
+const myList = ["pane", "tofu", "verdura", "biscotti", "ceci", "lenticchie", "patatine", "birra"];
+
+
+// per ogni elemento devo stapare un li in pagina
+
+// prendo elemento contenitore della mia lista
+const unorderList = document.getElementById("my_list");
+
+
+// creo un ciclo while per prendere i singoli elementi dell'array:
+// creo la variabile
+let i = 0; //la setto a zero, perché devo dare una condizione di partenza al ciclo successivo
+while(i < myList.length){ // metto la condizione per la quale il ciclo deve continuare
+    
+}
+
